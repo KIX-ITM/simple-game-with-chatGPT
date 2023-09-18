c84e8b7f0dd5e8dfb23701947cb32264d1923da7
KIX-ITM <anko1kuchi@gmail.com>
2023-09-18 15:03:19 +0900

    fix: debugのためflaskの内容修正

--- app/flask/test.py
+++ app/flask/test.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # テスト用
-@app.route('/')
+@application.route('/')
 def index():
     return '<h1>Hello Flask</h1>'
 
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,4 @@
-from flask.app import app
+from flask.test import application
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    application.run()
