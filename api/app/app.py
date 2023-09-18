a1538c10617a82d8eef2469562d9a2c1c70b832d
KIX-ITM <anko1kuchi@gmail.com>
2023-09-18 15:32:45 +0900

    fix: debugのためflaskの内容修正

--- api/app/app.py
+++ api/app/app.py
@@ -2,14 +2,14 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+application = FastAPI()
 
 # テスト用
-@app.get("/")
+@application.get("/")
 def read_root():
     return {"Hello": "World"}
 
 # テスト用
-@app.get("/items/{item_id}")
+@application.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
