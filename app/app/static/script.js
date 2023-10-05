5fffad5a929eb2eb523ff7108813a7564020af77
KIX-ITM <anko1kuchi@gmail.com>
2023-10-05 23:30:38 +0900

    change: html,cssにて各種画面を作成

--- app/app/static/script.js
+++ app/app/static/script.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', function(){
 	// チェックボックスのHTML要素を取得
 	let words = document.querySelectorAll("input[name=word]");
 
+    //選択出来るワードを2つまでに制限する
 	for(let word of words) {
 	    word.addEventListener('change',function(){
 		    let count = document.querySelectorAll('input[type="checkbox"]:checked').length;
