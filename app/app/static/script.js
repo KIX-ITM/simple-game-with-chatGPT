2a4c9f977a52f2c5f4ce4452775bb091aa959092
KIX-ITM <anko1kuchi@gmail.com>
2023-10-08 17:08:29 +0900

    change: html,cssにて各種画面を作成

--- app/app/static/script.js
+++ app/app/static/script.js
@@ -1,15 +1,23 @@
 window.addEventListener('DOMContentLoaded', function(){
 
 	// チェックボックスのHTML要素を取得
-	let words = document.querySelectorAll("input[name=word]");
+	const words = document.querySelectorAll("input[name=word]");
+//	const submit_answer = document.querySelector("input[name=submit-answer-checkbox");
+
+	let selected_num = 0;
 
     //選択出来るワードを2つまでに制限する
 	for(let word of words) {
 	    word.addEventListener('change',function(){
 		    let count = document.querySelectorAll('input[type="checkbox"]:checked').length;
+		    selected_num = count;
 		    if (count == 3) {
 		        this.checked = false;
 		    };
 	    });
 	}
+
+//	submit_answer.addEventListener('change',function(){
+//        if (selected_num != 2) this.checked = false;
+//	});
 });
--- app/app/views/index.py
+++ app/app/views/index.py
@@ -5,4 +5,25 @@ bp = Blueprint('index', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
+
     return render_template('index.html')
+
+# from flask import abort, redirect, url_for
+#
+# @app.route('/')
+# def index():
+# init作業
+# 3つの単語をランダムで抽出（正解と回答も）
+#     return render_template('hello.html', name=name)
+#
+# @app.route('/<name>/<nanido>')
+# def hello(name=None):
+# 難易度取得
+# ChatGPTに質問　→　日本語回答がリターン
+#     return redirect(url_for('nannido'))
+#
+# @app.route('/nannido/')
+# @app.route('/nannido/<name>')
+# def hello(name=None):
+#     return redirect(url_for('nannido'))
+#     return render_template('hello.html', name=name)
