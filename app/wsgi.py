fffa4b9c2d0fcf5fc90ac31d793fc0f232dd5577
KIX-ITM <anko1kuchi@gmail.com>
2023-10-18 21:46:11 +0900

    fix: wsgi.pyとuwsgi.iniを書き換え

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,5 @@
-from app import create_app
+from app import create_app as application
 
-app = create_app()
-app.run()
+if __name__ == '__main__':
+    application = application()
+    application.run()
