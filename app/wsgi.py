15c421f7e314f3dba8e962a1e699cadf3be5bc25
KIX-ITM <anko1kuchi@gmail.com>
2023-10-18 23:53:53 +0900

    fix: wsgi.pyを書き換え

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,5 +1,5 @@
 from app import create_app as application
 
 if __name__ == '__main__':
-    application = application(config_mode=None)
-    application.run()
+    app = application()
+    app.run()
