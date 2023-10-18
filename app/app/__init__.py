280d59313b317713e07d6d9afe1c26df8b184157
KIX-ITM <anko1kuchi@gmail.com>
2023-10-18 22:39:32 +0900

    fix: appにログ設定追加

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,5 +1,22 @@
 import logging
+from logging.config import dictConfig
 from flask import Flask
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
 def create_app(config_mode=None):
