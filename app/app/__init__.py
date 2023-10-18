fe94d2294d1fe71de94890d42263581e15f0485b
KIX-ITM <anko1kuchi@gmail.com>
2023-10-19 00:54:53 +0900

    fix: appの__init__.pyを書き換え

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -26,12 +26,8 @@ dictConfig({
 def create_app(config_mode='production'):
     app = Flask(__name__, instance_relative_config=True)
 
-    if config_mode == 'production':
-        # 本番の設定ファイルは環境変数で指定
-        app.config.from_envvar('APP_CONFIG_FILE_PATH')
-        logging.basicConfig(level=logging.INFO)
-    else:
-        # それ以外の設定はconfig.pyを指定
+    if config_mode != 'production':
+        # 本番以外のログ設定はtest_config.pyを指定
         config_object = 'test_config.' + config_mode
         app.config.from_object(config_object)
         logging.basicConfig(level=logging.DEBUG)
