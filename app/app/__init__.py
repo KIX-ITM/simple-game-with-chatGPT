cb71e5572b7df56fd5e8a9ac932ebbcb0ce9c505
KIX-ITM <anko1kuchi@gmail.com>
2023-10-19 23:56:37 +0900

    change: Flaskログ設定を一時的にDEBUGに変更

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -2,34 +2,15 @@ import logging
 from logging.config import dictConfig
 from flask import Flask
 
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'file': {
-            'format': '[%(asctime)s] [%(levelname)s] : %(message)s',
-        }
-    },
-    'handlers': {
-        'file': {
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'file',
-            'filename': '/home/app/app.log'
-        }
-    },
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['file']
-    },
-})
-
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_envvar('APP_CONFIG_FILE_PATH')
+    logging.basicConfig(filename='/home/app/app.log', level=logging.DEBUG, format='[%(asctime)s] [%(levelname)s] : %(message)s')
 
     from app.views import index, info
     app.register_blueprint(index.bp)
     app.register_blueprint(info.bp)
+    app.logger.info('This is an info message.')
 
     return app
